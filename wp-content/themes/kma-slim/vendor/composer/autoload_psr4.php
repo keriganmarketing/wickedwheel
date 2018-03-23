c66fc5908e0a7930535c4d5a55508583964db045
Daron Adkins <daron.adkins@gmail.com>
2018-03-23 11:16:03 -0500

    wip

--- wp-content/themes/kma-slim/vendor/composer/autoload_psr4.php
+++ wp-content/themes/kma-slim/vendor/composer/autoload_psr4.php
@@ -6,11 +6,15 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
+    'Symfony\\Component\\Translation\\' => array($vendorDir . '/symfony/translation'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
     'MetzWeb\\Instagram\\' => array($vendorDir . '/cosenary/instagram/src'),
+    'KeriganSolutions\\FacebookFeed\\' => array($vendorDir . '/kerigansolutions/fb-autoblog-wp/src'),
     'KeriganSolutions\\CPT\\' => array($vendorDir . '/kerigansolutions/cpt/src'),
     'Includes\\Modules\\' => array($baseDir . '/inc/modules'),
     'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
     'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
     'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
+    'Carbon\\' => array($vendorDir . '/nesbot/carbon/src/Carbon'),
 );
