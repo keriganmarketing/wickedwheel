45a891914b8998789b01888a2a6b2c0c55b2f493
programmer-man <bryan@kerigan.com>
2018-03-26 16:49:57 -0400

    header revisions

--- wp-content/themes/kma-slim/template-parts/sections/support-heading.php
+++ wp-content/themes/kma-slim/template-parts/sections/support-heading.php
@@ -1,15 +1,16 @@
 <div class="sticky-header-pad support"></div>
 <section class="support-header">
     <div class="tire-texture"></div>
-</section>
-<div class="container">
-    <div class="header-content">
-        <h1 class="title is-1 tandelle is-primary"><?php echo $headline; ?></h1>
-        <?php echo($subhead != '' ? '<p class="subtitle">' . $subhead . '</p>' : null); ?>
-        <?php if ('post' === get_post_type()) : ?>
-            <div class="entry-meta">
-                <?php //kmaslim_posted_on(); ?>
-            </div>
-        <?php endif; ?>
+    <div class="container">
+        <div class="header-content">
+            <h1 class="title is-1 tandelle is-primary"><?php echo $headline; ?></h1>
+            <?php echo($subhead != '' ? '<p class="subtitle">' . $subhead . '</p>' : null); ?>
+            <?php if ('post' === get_post_type()) : ?>
+                <div class="entry-meta">
+                    <?php //kmaslim_posted_on(); ?>
+                </div>
+            <?php endif; ?>
+        </div>
     </div>
-</div>
+</section>
+
