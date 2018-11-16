2ad7d6ca5b1e7e84f995140c789968e6b87faac1
Bryan Baird <bryan@kerigan.com>
2018-11-16 21:55:16 +0000

    local updates

--- wp-content/plugins/wordpress-seo/vendor/autoload_52.php
+++ wp-content/plugins/wordpress-seo/vendor/autoload_52.php
@@ -4,4 +4,4 @@
 
 require_once dirname(__FILE__) . '/composer'.'/autoload_real_52.php';
 
-return ComposerAutoloaderInit7ab96b2e8659d10000179534ced24395::getLoader();
+return ComposerAutoloaderInit8c8f8a97754b755b52ca9d931feaed91::getLoader();
