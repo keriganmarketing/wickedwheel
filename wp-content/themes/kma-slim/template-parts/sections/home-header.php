46368c6f02f091a84fc5d5aa0b4454b4ebc8a0fd
programmer-man <bryan@kerigan.com>
2018-11-16 17:18:25 -0500

    fix headline area

--- wp-content/themes/kma-slim/template-parts/sections/home-header.php
+++ wp-content/themes/kma-slim/template-parts/sections/home-header.php
@@ -12,7 +12,7 @@ use Includes\Modules\Slider\BulmaSlider;
         foreach($slides as $slide){
             $slider .= '<slide :id="'.number_format($i).'" image="'.$slide['photo'].'" >
                         <section class="slide-content">'
-                            . ($slide['headline'] != '' ? '<h2 class="title is-1 is-primary">'.$slide['headline'].'</h2>' : '')
+                            . ($slide['headline'] != '' ? $slide['headline'] : '')
                             . ($slide['caption'] != '' ? '<p class="slider-subtitle">'.$slide['caption'].'</p>' : '')
                             . ($slide['description'] != '' ? '<div class="slider-description">'.$slide['description'].'</div>' : '')
                             . ($slide['url'] != '' ? '<a class="button is-primary is-rounded has-shadow" href="'.$slide['url'].'">'
