3824c128a0434093a24cd6ea61c64d2c8af639ac
ToniSHernandez <toni@kerigan.com>
2021-07-14 09:18:30 -0500

    Added alt text to Trip Adiviso link block

--- wp-content/themes/kma-slim/template-parts/partials/tripadvisor-module.php
+++ wp-content/themes/kma-slim/template-parts/partials/tripadvisor-module.php
@@ -1,7 +1,7 @@
 <div class="card social-module tripadvisor has-text-centered">
     <div id="TA_selfserveprop74" class="TA_selfserveprop">
         <ul id="OHT8EDQDA" class="TA_links GFHAtN">
-            <li id="cNLqxvWNp7" class="qgMunzkCM"><a target="_blank" href="https://www.tripadvisor.com/"><img
+            <li id="cNLqxvWNp7" class="qgMunzkCM"><a target="_blank" alt="Link to Trip Advisor" href="https://www.tripadvisor.com/"><img
                             src="https://www.tripadvisor.com/img/cdsi/img2/branding/150_logo-11900-2.png"
                             alt="TripAdvisor"/></a></li>
         </ul>
