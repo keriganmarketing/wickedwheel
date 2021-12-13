3bdc81373801606c3bc05629333ce230dc1ccfed
Bryan Baird <23462829+programmer-man@users.noreply.github.com>
2021-12-13 10:25:58 -0600

    cleanup

--- wp-content/themes/kma-slim/vendor/kerigansolutions/cpt/src/templates/radio.php
+++
@@ -1,8 +0,0 @@
-<table width="100%" cellpadding="5px">
-    <tr>
-        <td width="15%" align="right" valign="top">
-            <label for="{field-name}">{field-label}</label>
-        </td>
-        <td>{multifields}</td>
-    </tr>
-</table>
