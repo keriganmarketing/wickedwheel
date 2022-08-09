019d7a5e55d31df30b7a884e11ed1507946fe10d
Bryan Baird <23462829+programmer-man@users.noreply.github.com>
2022-08-09 13:16:25 -0500

    cleanup repo

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:":k#)pNGokzjKN#TWoJjLEJ[1AN5s,!0Wl/^:#e[^200Gdic*><c5l6>fKM[g?u{z";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2033;}
